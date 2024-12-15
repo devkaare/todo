f8f5c529d192774bda260bedea4d3823fdc91e4b
devkaare <kaarehofferdal@gmail.com>
2024-12-15 22:56:57 +0100

    Feat

--- database/database_test.go
+++ database/database_test.go
@@ -1,13 +1,69 @@
 package database
 
 import (
-	// "context"
+	"context"
+	"log"
 	"testing"
+	"time"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-// func mustStartPostgresContainer() (func(context.Context) error, error)
+func mustStartPostgresContainer() (func(context.Context) error, error) {
+	var (
+		dbName = "database"
+		dbPwd  = "password"
+		dbUser = "user"
+	)
 
-// func TestMain(m *testing.M)
+	dbContainer, err := postgres.Run(
+		context.Background(),
+		"postgres:latest",
+		postgres.WithDatabase(dbUser),
+		postgres.WithUsername(dbUser),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	database = dbName
+	password = dbPwd
+	username = dbUser
+
+	dbHost, err := dbContainer.Host(context.Background())
+	if err != nil {
+		return dbContainer.Terminate, err
+	}
+
+	dbPort, err := dbContainer.MappedPort(context.Background(), "5432/tcp")
+	if err != nil {
+		return dbContainer.Terminate, err
+	}
+
+	host = dbHost
+	port = dbPort.Port()
+
+	return dbContainer.Terminate, err
+}
+
+func TestMain(m *testing.M) {
+	teardown, err := mustStartPostgresContainer()
+	if err != nil {
+		log.Fatalf("could not start postgres container: %v", err)
+	}
+
+	m.Run()
+
+	if teardown != nil && teardown(context.Background()) != nil {
+		log.Fatalf("could not teardown postgres container: %v", err)
+	}
+}
 
 func TestNew(t *testing.T) {
 	srv := New()
